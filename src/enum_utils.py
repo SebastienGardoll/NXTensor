a52947976068c3e17915ea621c87e7daf8eb3965
Sébastien Gardoll <sebastien.gardoll@ipsl.fr>
2019-04-03 16:11:19 +0200

    add new coordinate formats ; rename RegionGEometry into SelectionShape

--- src/enum_utils.py
+++ src/enum_utils.py
@@ -18,14 +18,15 @@ class TimeResolution(Enum):
   MINUTE    = 'minute'
   SECOND    = 'second'
 
-
 class CoordinateFormat(Enum):
 
-  DECREASING_DEGREE_NORTH = 'decreasing_degree_north'
-  AMERICAN_DEGREE_EAST    = 'american_degree_east'
+  DECREASING_DEGREE_NORTH = 'decreasing_degree_north' # From   90° to  -90°
+  INCREASING_DEGREE_NORTH = 'increasing_degree_north' # From  -90° to   90°
+  AMERICAN_DEGREE_EAST    = 'american_degree_east'    # From    0° to  360°
+  EUROPEAN_DEGREE_EAST    = 'european_degree_east'    # From -180° to  180°
 
 class DbFormat(Enum):
   CSV = 'csv'
 
-class RegionGeometry(Enum):
+class SelectionShape(Enum):
   SQUARE = 'square'
