4f49b6b239658b7060501c5bc20c14d357bf8f7c
Sébastien Gardoll <sebastien@gardoll.fr>
2020-11-06 15:42:51 +0100

    fix channels first

--- nxtensor/utils/image_utils.py
+++ nxtensor/utils/image_utils.py
@@ -13,15 +13,17 @@ from typing import Tuple, Sequence
 from matplotlib import pyplot as plt
 
 
-def display_channels(tensor: np.ndarray, variable_names: Sequence, title: str,
+def display_channels(tensor: np.ndarray, variable_names: Sequence, title: str, is_channels_last: bool,
                      plot_size: Tuple[float, float] = (20, 2.5), has_to_invert_latitudes: bool = False) -> None:
-    reshaped_tensor = tensor.swapaxes(0, 2)
+    if is_channels_last:
+      # Brings back the channels from the last dimension to the first.
+      tensor = tensor.swapaxes(0,2).swapaxes(1, 2)
     if has_to_invert_latitudes:
-        reshaped_tensor = reshaped_tensor.swapaxes(1, 2)  # Set the latitude as vertical axis.
+      tensor = tensor.swapaxes(1, 2)
     plt.figure(figsize=plot_size)
-    nb_channel = tensor.shape[-1]
+    nb_channel = tensor.shape[0]
     for channel_id in range(0, nb_channel):
-        channel = reshaped_tensor[channel_id]
+        channel = tensor[channel_id]
         plt.subplot(1, nb_channel, (channel_id+1))
         plt.title(variable_names[channel_id], {'fontsize': 14})
         # Remove ticks from the x and y axes
